c19d27b237a14ec345db65f027e5e8b523586a9f
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2020-06-10 17:32:36 -0500

    Add files via upload

--- test_coupon_calculations.py
+++ test_coupon_calculations.py
@@ -4,12 +4,12 @@ from coupon_calculations import calculate_price
 
 class FunctionTestCase(unittest.TestCase):
     def test_price_under_ten(self):
-        self.assertAlmostEqual(calculate_price(8, 5, 10), 8.81, places=2)
-        self.assertAlmostEqual(calculate_price(8, 5, 15), 8.65, places=2)
-        self.assertAlmostEqual(calculate_price(8, 5, 20), 8.49, places=2)
-        self.assertAlmostEqual(calculate_price(8, 10, 10), 5.95, places=2)
-        self.assertAlmostEqual(calculate_price(8, 10, 15), 5.95, places=2)
-        self.assertAlmostEqual(calculate_price(8, 10, 20), 5.95, places=2)
+        self.assertAlmostEqual(calculate_price(8, 5, 10), 8.28, places=2)
+        self.assertAlmostEqual(calculate_price(8, 5, 15), 7.86, places=2)
+        self.assertAlmostEqual(calculate_price(8, 5, 20), 7.43, places=2)
+        self.assertAlmostEqual(calculate_price(8, 10, 10), 2.98, places=2)
+        self.assertAlmostEqual(calculate_price(8, 10, 15), 2.56, places=2)
+        self.assertAlmostEqual(calculate_price(8, 10, 20), 2.13, places=2)
 
 
 if __name__ == '__main__':
